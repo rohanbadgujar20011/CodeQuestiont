8f5f430cfa53cc3b9b4a076b21138f140e1fad53
Rohan Badgujar <80501166+rohanbadgujar20011@users.noreply.github.com>
2022-09-17 11:49:03 +0530

    Final commit

--- Week6/deleteNode.py
+++ Week6/deleteNode.py
@@ -1,29 +1,6 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-def printList(node):
-  while (node != None):
-    print(node.data, end=' ');
-    node = node.next;
-    
-def insertEnd(head, data):
-  new_node = Node(data)
-  if head is None:
-    head = new_node
-    return head;
-    
-  last = head
-  while (last.next):
-    last = last.next
-    
-  last.next =  new_node
-  return head;
 def deleteNode(node):
-   def deleteNode(node):
-    if(node==None or node.next==None):
-      return
+  if(node==None or node.next==None):
+    return
   temp=node
   while (node.next !=None):
     temp=node
@@ -31,25 +8,3 @@ def deleteNode(node):
     temp.data=node.data
   temp.next=None
 
-def main():
-    t = int(input().strip());
-    for i in range(t):
-        head = None;
-        n = int(input().strip());
-        k = 0;
-        if(n!=0):
-            inp = input().strip().split();
-            print(inp)
-            for j in inp:
-                head = insertEnd(head,int(j.strip()));
-            k = int(input().strip());
-            node = head;
-        if(k>0):
-            while(k>0):
-                node = node.next;
-                k = k-1;
-            deleteNode(node);
-            printList(head);
-            print();
-if __name__ == "__main__":
-  main();
